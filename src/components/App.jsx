475f66cbaff532f91b4a3354a40454cadbe21ec6
aricallen <aric.allen2@gmail.com>
2019-11-10 14:01:58 -0800

    home page and router setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
 
 export function App() {
-  return <div>Hello React</div>;
+  return (
+    <Router basename="/">
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
 }
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Wrapper = styled('div')`
+  background-color: blue;
+`;
+
+export const Home = () => {
+  return <Wrapper>Home page</Wrapper>;
+};
