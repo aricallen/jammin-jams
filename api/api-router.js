e426cdb4f5e1cf76acdcc26a5d691ddd63a4fabb
aricallen <aric.allen2@gmail.com>
2019-11-25 21:53:28 -0800

    add email notification

--- api/api-router.js
+++ api/api-router.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { serialize } = require('./utils/db-helpers');
+const { sendEmail, serializeForEmail } = require('./utils/email-helpers');
 
-const { serialize, deserialize } = require('./utils/db-helpers');
+const recipients = ['aric.allen2@gmail.com','celestetretto@gmail.com'];
 
 const apiRouter = express.Router();
 
@@ -12,6 +14,9 @@ apiRouter.post('/waitlist', async (req, res) => {
   try {
     const result = await db.query('INSERT INTO waitlist SET ?', row);
     console.log('inserted!', JSON.stringify(result));
+    recipients.forEach((to) => {
+      sendEmail({ message: serializeForEmail(row), to, subject: 'Someone Joined the waitlist for Jammin Jams!' });
+    });
     res.send({ message: 'inserted!' });
   } catch (err) {
     res.status(400).send({
---
+++ api/utils/email-helpers.js
@@ -0,0 +1,33 @@
+const email = require('emailjs/email');
+
+const { EMAIL_USERNAME, EMAIL_PASSWORD, EMAIL_HOST } = process.env;
+
+const server = email.server.connect({
+  user: EMAIL_USERNAME,
+  password: EMAIL_PASSWORD,
+  host: EMAIL_HOST,
+  ssl: true,
+});
+
+const sendEmail = ({ message, to, subject }) => {
+  server.send(
+    { text: message, from: 'solstice.sebastian@gmail.com', to, subject, attachment: [] },
+    (err, sentMessage) => {
+      if (err) {
+        console.error('unable to send email');
+      } else {
+        console.log('message sent!', sentMessage);
+      }
+    }
+  );
+};
+
+const serializeForEmail = (row) => {
+  return Object.entries(row).reduce((acc, curr) => {
+    const [field, value] = curr;
+    acc.push(`${field}: ${value}`);
+    return acc;
+  }, []).join('\n');
+};
+
+module.exports = { sendEmail, serializeForEmail };
