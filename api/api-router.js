97e1990a24e79d73dde18703b262542788a62a61
aricallen <aric.allen2@gmail.com>
2019-12-01 13:48:25 -0800

    admin router and GET routes

--- api/api-router.js
+++ api/api-router.js
@@ -1,31 +1,10 @@
 const express = require('express');
-const { serialize, getConnection } = require('./utils/db-helpers');
-const { sendEmail, serializeForEmail } = require('./utils/email-helpers');
-
-const recipients = ['aric.allen2@gmail.com','celestetretto@gmail.com'];
+const { adminRouter } = require('./admin-router');
 
 const apiRouter = express.Router();
 
-apiRouter.get('/status', (req, res) => res.send({ status: 'ok' }));
+apiRouter.use('/admin', adminRouter);
 
-apiRouter.post('/waitlist', async (req, res) => {
-  const { body } = req;
-  const connection = await getConnection();
-  const row = serialize(body);
-  try {
-    const result = await connection.query('INSERT INTO waitlist SET ?', row);
-    console.log('inserted!', JSON.stringify(result));
-    recipients.forEach((to) => {
-      sendEmail({ message: serializeForEmail(row), to, subject: 'Someone Joined the waitlist for Jammin Jams!' });
-    });
-    res.send({ message: 'inserted!' });
-    connection.release();
-  } catch (err) {
-    res.status(400).send({
-      error: err,
-      message: 'unable to insert',
-    });
-  }
-});
+apiRouter.get('/status', (req, res) => res.send({ status: 'ok' }));
 
 module.exports = { apiRouter };
--- api/utils/db-helpers.js
+++ api/utils/db-helpers.js
@@ -2,9 +2,9 @@ const { snakeCase, camelCase } = require('lodash');
 const pmysql = require('promise-mysql');
 
 const { DATABASE_URL } = process.env;
-const [user, password, host, _dbPort, database] = DATABASE_URL.replace('mysql://', '').split(/\/|:|@/g);
 
-const getConnection = async () => {
+const getConnection = async (dbUrl = DATABASE_URL) => {
+  const [user, password, host, _dbPort, database] = dbUrl.replace('mysql://', '').split(/\/|:|@/g);
   const connection = await pmysql.createConnection({ host, user, password, database });
   return connection;
 };
