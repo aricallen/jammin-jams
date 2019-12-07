df433b1c00145f32cde322ec24913455c31a4fdc
aricallen <aric.allen2@gmail.com>
2019-12-07 10:57:12 -0800

    add spinner to button

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { omit } from 'lodash';
 import styled from '@emotion/styled';
 import { spacing, pallet, font } from '../../constants/style-guide';
+import { Spinner } from './Spinner';
 
-export const Button = styled('button')`
+const ButtonElem = styled('button')`
   cursor: pointer;
   padding: ${spacing.regular}px;
   background-color: ${pallet.strawberry};
   border-radius: ${spacing.regular}px;
   font-size: ${font.size.regular}px;
   font-weight: ${font.weight.bold};
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   &:active,
   &:focus,
@@ -18,3 +24,34 @@ export const Button = styled('button')`
     box-shadow: 0 0 4px ${pallet.strawberry};
   }
 `;
+
+const SpinnerWrapper = styled('div')`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TextWrapper = styled('span')`
+  opacity: ${p => (p.isHidden ? 0 : 1)};
+`;
+
+export const Button = props => {
+  const { children, isBusy } = props;
+  const buttonProps = omit(props, ['children', 'isBusy']);
+  const buttonRef = useRef();
+  const width = buttonRef.current && buttonRef.current.width;
+  const height = buttonRef.current && buttonRef.current.height;
+  return (
+    <ButtonElem ref={buttonRef} {...buttonProps}>
+      {isBusy ? (
+        <SpinnerWrapper width={width} height={height}>
+          <Spinner />
+        </SpinnerWrapper>
+      ) : null}
+      <TextWrapper isHidden={isBusy}>{children}</TextWrapper>
+    </ButtonElem>
+  );
+};
