345d77f77277db7540ae489be055e28183d30670
aricallen <aric.allen2@gmail.com>
2019-12-26 12:01:51 -0800

    fix deploy

--- scripts/prepare-deploy.sh
+++ scripts/prepare-deploy.sh
@@ -7,7 +7,7 @@ mkdir $BUILD_DIR
 cp -R src/assets dist/
 cp -R src/styles dist/
 # build dir for deploying
-cp -R api $BUILD_DIR/
+cp -R server $BUILD_DIR/
 cp -R dist $BUILD_DIR/
 cp Procfile $BUILD_DIR/
 cp scripts/_package.json $BUILD_DIR/package.json
