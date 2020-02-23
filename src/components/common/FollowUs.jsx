650e62339a2240a6ab28c88f6aea653d010d6ee7
aricallen <aric.allen2@gmail.com>
2020-02-23 13:54:50 -0800

    social icons and FollowUs comp

--- src/components/common/FollowUs.jsx
+++ src/components/common/FollowUs.jsx
@@ -1,6 +1,48 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { animation, pallet } from '../../constants/style-guide';
+import InstagramIcon from '../../assets/icons/instagram.svg';
+import MailIcon from '../../assets/icons/mail.svg';
+import SoundcloudIcon from '../../assets/icons/soundcloud.svg';
+import GithubIcon from '../../assets/icons/github.svg';
+
+const Wrapper = styled('div')`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+`;
+
+const IconLink = styled('a')`
+  svg circle,
+  svg g.circle-fill {
+    transition: fill ${animation};
+  }
+
+  &:hover {
+    cursor: pointer;
+    svg circle,
+    svg g.circle-fill {
+      fill: ${pallet.light.strawberry};
+    }
+  }
+`;
 
 export const FollowUs = () => {
-  return <div>social icons...</div>;
+  return (
+    <Wrapper>
+      <IconLink target="_blank" href="https://www.instagram.com/jmn_jams/">
+        <InstagramIcon />
+      </IconLink>
+      <IconLink target="_blank" href="mailto:jam@jmnjams.com">
+        <MailIcon />
+      </IconLink>
+      <IconLink target="_blank" href="https://github.com/aricallen/jmnjams">
+        <GithubIcon />
+      </IconLink>
+      <IconLink target="_blank" href="https://soundcloud.com/solstice-sebastian">
+        <SoundcloudIcon />
+      </IconLink>
+    </Wrapper>
+  );
 };
