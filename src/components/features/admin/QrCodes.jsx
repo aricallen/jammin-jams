18fe4c8618ea13402a20c62da204cafab6fde3a6
aricallen <aric.allen2@gmail.com>
2020-05-03 19:26:10 -0700

    qr-code generator

--- src/components/features/admin/QrCodes.jsx
+++ src/components/features/admin/QrCodes.jsx
@@ -1,5 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
+// import { pallet, spacing } from '../../../constants/style-guide';
+import { Content, Header1, Header2, Section } from '../../common/Structure';
+import { Button } from '../../common/Button';
+import { FormInput } from '../../common/Forms';
+import { generate } from '../../../services/qr-code';
+import { Spinner } from '../../common/Spinner';
+
+const Image = styled('img')``;
 
 export const QrCodes = () => {
-  return <div>download and generate new codes here...</div>;
+  const [url, setUrl] = useState('');
+  const [isBusy, setIsBusy] = useState(false);
+  const [dataUrl, setDataUrl] = useState('');
+
+  const onClick = async () => {
+    setIsBusy(true);
+    const generatedDataUrl = await generate(url);
+    setDataUrl(generatedDataUrl);
+    setIsBusy(false);
+  };
+
+  return (
+    <Content>
+      <Header1>QR Code Generator</Header1>
+      <Section>
+        <FormInput
+          name="url"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          label="Url"
+          isRequired={true}
+        />
+      </Section>
+      <Section>
+        <Button onClick={onClick} isBusy={isBusy}>
+          Generate
+        </Button>
+      </Section>
+
+      <Section>
+        <Header2>Output will appear below</Header2>
+        {isBusy && <Spinner variante="large" />}
+        {dataUrl && <Image src={dataUrl} />}
+      </Section>
+    </Content>
+  );
 };
