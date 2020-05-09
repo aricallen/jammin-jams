3a6f1d2bfcf44590bbea0fb59bf3730b561a712a
aricallen <aric.allen2@gmail.com>
2020-05-09 16:05:03 -0700

    meta tags for posts

--- src/components/common/MetaTags.jsx
+++ src/components/common/MetaTags.jsx
@@ -1,18 +1,26 @@
 import PropTypes from 'prop-types';
 
+const cleanPath = (path) => path.replace(/^\//, '').replace(/^https:\/\/jmnjams.com\//, '');
+
+const fullUrl = (path) => `https://jmnjams.com/${cleanPath(path)}`;
+
 export const MetaTags = (props) => {
-  const { title, description, path } = props;
+  const { title, description, path, ogImage } = props;
   const computedTitle = `${title} | Jam Subscription Service | Jammin' Jams`;
-  const url = `https://jmnjams.com/${path.replace(/^\//, '')}`;
   document.head.querySelector('title').innerText = computedTitle;
 
   document.head.querySelector('meta[property="og:title"]').setAttribute('content', computedTitle);
-  document.head.querySelector('meta[property="og:url"]').setAttribute('content', url);
+  document.head.querySelector('meta[property="og:url"]').setAttribute('content', fullUrl(path));
   document.head
     .querySelector('meta[property="og:description"]')
     .setAttribute('content', description);
 
   document.head.querySelector('meta[name="description"]').setAttribute('content', description);
+  if (ogImage) {
+    document.head
+      .querySelector('meta[property="og:image"]')
+      .setAttribute('content', fullUrl(ogImage));
+  }
   return null;
 };
 
